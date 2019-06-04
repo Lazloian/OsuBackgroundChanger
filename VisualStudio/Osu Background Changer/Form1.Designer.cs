ce0c5fc420cf9a152ff864ebf6054fb945ab647f
Henry Silva <henry.silva.999@gmail.com>
2019-06-04 16:50:19 -0700

    Add files via upload

---
+++ VisualStudio/Osu Background Changer/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Osu_Background_Changer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(152, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(12, 92);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(134, 13);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Osu Songs Folder Path";
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(12, 124);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(134, 13);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "Backup Folder Path";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(152, 121);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(195, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextChanged += new System.EventHandler(this.TextBox4_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(152, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Replace Backgrounds";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Location = new System.Drawing.Point(12, 152);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(134, 13);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.Text = "Backgrounds Folder Path";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(152, 149);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(195, 20);
+            this.textBox6.TabIndex = 6;
+            this.textBox6.TextChanged += new System.EventHandler(this.TextBox6_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(152, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Backup Backgrounds";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 409);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Background Changer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ VisualStudio/Osu Background Changer/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Osu_Background_Changer
+{
+    public partial class Form1 : Form
+    {
+
+        string songs_folder = @"D:\WeebShit\Songs";
+        string backup_folder = @"C:\Users\Henry\Desktop\Backup";
+        string backgrounds_folder = @"D:\Media\Backgrounds";
+
+        string[] songs;
+        string[] backgrounds;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            songs_folder = @textBox1.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox4_TextChanged(object sender, EventArgs e)
+        {
+            backup_folder = @textBox4.Text;
+        }
+
+        private void TextBox6_TextChanged(object sender, EventArgs e)
+        {
+            backgrounds_folder = @textBox6.Text;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            songs = Directory.GetDirectories(songs_folder, "*");
+
+            foreach (string song in songs)
+            {
+                Directory.CreateDirectory(backup_folder + @"\" + new DirectoryInfo(song).Name);
+
+                string[] names = Get_BG_Names(song);
+
+                foreach (string name in names)
+                {
+                    //Console.WriteLine(name);
+
+                    string[] bg = Directory.GetFiles(song, name);
+
+                    foreach (string picture in bg)
+                    {
+                        File.Copy(picture, backup_folder + @"\" + new DirectoryInfo(song).Name + @"\" + name, true);
+                    }
+                }
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            songs = Directory.GetDirectories(songs_folder, "*");
+            backgrounds = Directory.GetFiles(backgrounds_folder, "*");
+
+            Random rand = new Random();
+
+            foreach (string song in songs)
+            {
+
+                string[] names = Get_BG_Names(song);
+
+                foreach (string name in names)
+                {
+                    //Console.WriteLine(name);
+
+                    string[] bg = Directory.GetFiles(song, name);
+
+                    foreach (string picture in bg)
+                    {
+                        File.Copy(backgrounds[rand.Next(backgrounds.Length)], song + @"\" + name, true);
+                    }
+                }
+            }
+        }
+
+        private string[] Get_BG_Names(string song)
+        {
+            List<string> names = new List<string>();
+            string[] osu_files = Directory.GetFiles(song, "*.osu");
+
+            //Console.WriteLine(song);
+
+            foreach (string file in osu_files)
+            {
+                string[] lines = File.ReadAllLines(file);
+
+                foreach (string line in lines)
+                {
+                    if (line.Length > 5 && line.Substring(0,5).Equals("0,0,\""))
+                    {
+                        int firstq = line.IndexOf("\"");
+                        int lastq = line.LastIndexOf("\"");
+
+                        string current_name = line.Substring(firstq + 1, lastq - firstq - 1);
+
+                        bool is_unique = true;
+
+                        if (names.Count != 0)
+                        {
+                            foreach (string name in names)
+                            {
+                                if (name.Equals(current_name))
+                                {
+                                    is_unique = false;
+                                }
+                            }
+
+                            if (is_unique)
+                            {
+                                names.Add(current_name);
+                            }
+                        }
+                        else
+                        {
+                            names.Add(current_name);
+                        }
+                    }
+                }
+            }
+            return names.ToArray();
+        }
+    }
+}
